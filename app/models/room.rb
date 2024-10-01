e34d7d500dbef4b50124e061fa2aeb942cdb00e6
Anastasiia Kovalenko <n.kovalenko@sloboda-studio.com>
2024-10-01 15:58:48 +0200

    Added markup for message body block

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,4 +3,9 @@ class Room < ApplicationRecord
   scope :public_rooms, -> {where(is_private: false)}
   after_create_commit {broadcast_append_to "rooms" }
   has_many :messages
+  has_many :users, through: :messages
+
+  def unique_users_count
+    users.distinct.count
+  end
 end
