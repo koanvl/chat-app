43a2930aa77c7c65d440aaad5472ff97056efe05
Anastasiia Kovalenko <n.kovalenko@sloboda-studio.com>
2024-10-01 13:06:48 +0200

    Added flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,70 @@
 module.exports = {
   content: [
     './app/views/**/*.html.erb',
+    './app/views/**/*.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
-  ]
+    './app/javascript/**/*.js',
+    './node_modules/flowbite/**/*.js'
+  ],
+  theme: {
+    extend: {
+      screens: {
+        xs: '480px',
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+      },      
+    },
+    
+    fontFamily: {
+      'body': [
+        'Lato',
+        'Varela Round',
+        'Inter',
+        'ui-sans-serif',
+        'system-ui',
+        '-apple-system',
+        'system-ui',
+        'Segoe UI',
+        'Roboto',
+        'Helvetica Neue',
+        'Arial',
+        'Noto Sans',
+        'sans-serif',
+        'Apple Color Emoji',
+        'Segoe UI Emoji',
+        'Segoe UI Symbol',
+        'Noto Color Emoji'
+      ],
+      'sans': [
+        'Inter',
+        'ui-sans-serif',
+        'system-ui',
+        '-apple-system',
+        'system-ui',
+        'Segoe UI',
+        'Roboto',
+        'Helvetica Neue',
+        'Arial',
+        'Noto Sans',
+        'sans-serif',
+        'Apple Color Emoji',
+        'Segoe UI Emoji',
+        'Segoe UI Symbol',
+        'Noto Color Emoji'
+      ],
+      'heading': [
+        'Noto Serif',
+        'serif'
+      ]
+      
+    },    
+    
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
+
